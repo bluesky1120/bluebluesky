7777860bbf8cc24cdb89a3ab5c7e484917fa682a
baidu2018 <104385608@qq.com>
2018-10-03 12:36:11 +0800

    v1.4 master

--- app/src/main/java/com/example/baidu2018/mygithubtest/MainActivity.java
+++ app/src/main/java/com/example/baidu2018/mygithubtest/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this,"这是Clone的版本",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"这是 master1本地 的重新修改版本33333334446666",Toast.LENGTH_SHORT).show();
-        Toast.makeText(this,"这是 branch2 的版本重新修改232323点点滴滴333337777",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"这是 branch2 的版本重新修改232323点点滴滴3333377773",Toast.LENGTH_SHORT).show();
     }
 }
