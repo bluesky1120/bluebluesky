344cead54f745838535415fabaf4a3d0c5c86104
baidu2018 <104385608@qq.com>
2018-10-03 11:38:25 +0800

    v1.1 这是Clone提交的

--- app/src/main/java/com/example/baidu2018/mygithubtest/MainActivity.java
+++ app/src/main/java/com/example/baidu2018/mygithubtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.baidu2018.mygithubtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"这是Clone的版本",Toast.LENGTH_SHORT).show();
     }
 }
