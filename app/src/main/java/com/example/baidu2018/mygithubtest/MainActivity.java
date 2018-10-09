df24ea510426d7b7cc4381032b9ed6ef713c3e6f
v_wangjingxian <1043865608@qq.com>
2018-10-09 16:50:43 +0800

    v9.0

--- app/src/main/java/com/example/baidu2018/mygithubtest/MainActivity.java
+++ app/src/main/java/com/example/baidu2018/mygithubtest/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this,"这是Clone的版本",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"这是 111master1本地 的重新修改版本333333314446666dafadfsdf",Toast.LENGTH_SHORT).show();
         Toast.makeText(this,"这是 master2本地 123",Toast.LENGTH_SHORT).show();
-        Toast.makeText(this,"这是 branch2 的版本重新修改232323点点滴滴3333377773",Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"这是 branch2 的版本重新修改232323点点滴滴333337777399999",Toast.LENGTH_SHORT).show();
     }
 }
